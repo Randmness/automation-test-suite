a6023c71ee1b1a9ca8268e742f3e7f7eed0f7511
Randmness <mrand86@gmail.com>
2021-08-25 21:45:10 -0400

    Adding simple driver validation

---
+++ src/test/java/computers/TestSteps.java
@@ -0,0 +1,57 @@
+package computers;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class TestSteps {
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver",
+                Paths.get("src/test/resources/drivers/mac/chromedriver").toString());
+
+        if (driver == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+        }
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() {
+        if (driver!=null) {
+            driver.close();
+            driver.quit();
+        }
+    }
+
+    @Given("Navigate to Computers database")
+    public void navigateToPageForgotPassword() {
+        driver.navigate().to("http://computer-database.herokuapp.com/computers");
+    }
+
+    @Then("Application shows current entries.")
+    public void applicationDisplaysTheMessageOutputCode() {
+        String expectedMessage = " computers found";
+        String actualMessage = driver.findElement(By.xpath("/html/body/section/h1")).getText();
+
+        assertThat(actualMessage, endsWith(expectedMessage));
+    }
+}
---
+++ src/test/resources/features/ReadDatabase.feature
@@ -0,0 +1,7 @@
+@read
+Feature: Initial navigation to the home/initial page.
+
+  Scenario: Application loads successfully
+    Given Navigate to Computers database
+    Then Application shows current entries.
+
